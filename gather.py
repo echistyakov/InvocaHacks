09144a85742fdd0b9103b62da4e7e2ff22fa7b53
Jack Chistyakov <jack.chistyakov@outlook.com>
2016-08-27 13:37:21 -0700

    data points added

--- gather.py
+++ gather.py
@@ -1,6 +1,7 @@
 import datetime
 import dateutil.relativedelta
 import praw
+import requests
 import sys
 '''
 Usage:
@@ -12,10 +13,32 @@ Format of printed comments:
 
 
 def is_comment_valid(comment):
-	if comment.find("http") == -1 and len(comment) > 1 and comment.find("[deleted]") == -1 and comment.find("[removed]") == -1:
-		return True
-	else:
-		return False
+    if comment.find("http") == -1 and len(comment) > 1 and comment.find("[deleted]") == -1 and comment.find("[removed]") == -1:
+        return True
+    else:
+        return False
+
+
+def analyse_text(text):
+    req = requests.post(url='http://text-processing.com/api/sentiment/',
+                        data={'text': text})
+
+    # Convert decision to 1/0/-1 scale
+    if req.json()['label'] == 'pos':
+        decision = 1
+    elif req.json()['label'] == 'neg':
+        decision = -1
+    elif req.json()['label'] == 'neutral':
+        decision = 0
+
+    # Get a tuple of probabilities
+    pos_prob = req.json()['probability']['pos']
+    neg_prob = req.json()['probability']['neg']
+    neu_prob = req.json()['probability']['neutral']
+
+    probs = (pos_prob, neu_prob, neg_prob)
+
+    return (decision, probs)
 
 
 def run_script():
@@ -40,8 +63,13 @@ def run_script():
             if is_comment_valid(c.body):
                 c_body = c.body.replace('\n', ' ')
                 hours_and_minutes = str(r_time.hours * 60 + r_time.minutes)
+                analysis = analyse_text(c_body)
 
-                line = str(hours_and_minutes) + " " + c_body.encode("ascii", "ignore") + "\n"
+                line = "{} {} {} {} {}\n".format(str(hours_and_minutes),
+                                                 str(analysis[0]),
+                                                 str(analysis[1][0]),
+                                                 str(analysis[1][1]),
+                                                 str(analysis[1][2]))
                 out_file.write(line)
 
     out_file.close()
